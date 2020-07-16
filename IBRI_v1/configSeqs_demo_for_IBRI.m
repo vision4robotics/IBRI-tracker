770fbf2a0aa1daa0fd302804b51fef82731287ef
vision4robotics <fuchanghong_008@163.com>
2020-07-16 14:28:37 +0800

    update

---
+++ IBRI_v1/configSeqs_demo_for_IBRI.m
@@ -0,0 +1,6 @@
+function seqs=configSeqs_demo_for_IBRI
+
+seqUAVDT_S0601 = {struct('name','S0601','path','.\seq\S0601\','startFrame',1,'endFrame',82,'nz',6,'ext','jpg','init_rect',[0,0,0,0])};
+seqUAVDT_S1001 = {struct('name','S1001','path','.\seq\S1001\','startFrame',1,'endFrame',353,'nz',6,'ext','jpg','init_rect',[0,0,0,0])};
+
+seqs = seqUAVDT_S0601; 
